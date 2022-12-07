a9e0057990d63a6083397ea32b3fd755b435193d
ishaqadhel <ishaq.adhel@gmail.com>
2022-12-07 20:46:45 +0700

    feat: disable email sent

--- src/pages/api/trx/create-many-by-id.ts
+++ src/pages/api/trx/create-many-by-id.ts
@@ -2,8 +2,6 @@ import { Prisma } from '@prisma/client';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { User } from 'next-auth';
 
-import { sendMail } from '@/lib/email.server';
-import { numberWithCommas } from '@/lib/helper';
 import { prisma } from '@/lib/prisma';
 import requireSession from '@/lib/require-session.server';
 
@@ -33,36 +31,36 @@ async function handler(req: NextApiRequest, res: NextApiResponse, user: User) {
         })),
       });
 
-      const users = await prisma.user.findMany();
+      // const users = await prisma.user.findMany();
 
-      for (const transaction of transactionData) {
-        const subject = `Request uang dari ${user.name}`;
-        const text = `Hai! ${
-          user.name
-        } baru saja melakukan request uang sebesar Rp ${numberWithCommas(
-          transaction.amount
-        )} dengan keterangan: ${description}. Silakan melakukan pembayaran di https://lhoks.thcl.dev/trx/${
-          user.id
-        }`;
-        const html = `
-          <p>Hai!</p><p><strong>${
-            user.name
-          }</strong> baru saja melakukan request uang sebesar <strong>
-            Rp ${numberWithCommas(transaction.amount)}
-          </strong> dengan keterangan: <strong>${description}</strong>. Silakan melakukan pembayaran di <a href='https://lhoks.thcl.dev/trx/${
-          user.id
-        }'>link berikut.</a></p>
-        `;
+      // for (const transaction of transactionData) {
+      //   const subject = `Request uang dari ${user.name}`;
+      //   const text = `Hai! ${
+      //     user.name
+      //   } baru saja melakukan request uang sebesar Rp ${numberWithCommas(
+      //     transaction.amount
+      //   )} dengan keterangan: ${description}. Silakan melakukan pembayaran di https://lhoks.thcl.dev/trx/${
+      //     user.id
+      //   }`;
+      //   const html = `
+      //     <p>Hai!</p><p><strong>${
+      //       user.name
+      //     }</strong> baru saja melakukan request uang sebesar <strong>
+      //       Rp ${numberWithCommas(transaction.amount)}
+      //     </strong> dengan keterangan: <strong>${description}</strong>. Silakan melakukan pembayaran di <a href='https://lhoks.thcl.dev/trx/${
+      //     user.id
+      //   }'>link berikut.</a></p>
+      //   `;
 
-        sendMail({
-          to: users.find((user) => user.id === transaction.id)?.email as string,
-          toName: users.find((user) => user.id === transaction.id)
-            ?.name as string,
-          subject,
-          text,
-          html,
-        });
-      }
+      //   sendMail({
+      //     to: users.find((user) => user.id === transaction.id)?.email as string,
+      //     toName: users.find((user) => user.id === transaction.id)
+      //       ?.name as string,
+      //     subject,
+      //     text,
+      //     html,
+      //   });
+      // }
 
       return res.status(201).json({ message: 'Transactions created' });
     } catch (error) {
